d89c04fab170c04ee5e201a1ed6a0fd308906003
RyanPier <136802720+RyanPier@users.noreply.github.com>
2025-09-17 23:29:23 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 <Box display={{ md: 'flex' }}>
   <Box flexGrow={1}>
     <Heading as="h2" variant="page-title">
-      Takuya Matsuyama
+      Nhat An Vo
     </Heading>
-    <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+    <p>Office Clerk</p>
   </Box>
   <Box
     flexShrink={0}
@@ -18,7 +18,7 @@
       maxWidth="100px"
       display="inline-block"
       borderRadius="full"
-      src="/images/takuya.jpg"
+      src="/images/nhatanvo.jpg" // Note: You'll need to upload a profile picture named 'nhatanvo.jpg' to the 'public/images/' folder later.
       alt="Profile image"
     />
   </Box>
