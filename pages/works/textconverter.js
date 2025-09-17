2bee6c7588a5b3e39ea32f1152eabe6b97583cd0
RyanPier <136802720+RyanPier@users.noreply.github.com>
2025-09-17 23:34:33 +1000

    Create textconverter.js

---
+++ pages/works/textconverter.js
@@ -0,0 +1,33 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => (
+  <Layout title="Text Converter">
+    <Container>
+      <Title>
+        Text Converter <Badge>2025</Badge>
+      </Title>
+      <P>
+        A simple and easy-to-use website for various text conversions.
+      </P>
+      <List ml={4} my={4}>
+        <ListItem>
+          <Meta>Website</Meta>
+          <Link href="https://trjbuizc.manus.space/">
+          https://trjbuizc.manus.space/ <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Platform</Meta>
+          <span>Web</span>
+        </ListItem>
+      </List>
+    </Container>
+  </Layout>
+)
+
+export default Work
+export { getServerSideProps } from '../../components/chakra'
