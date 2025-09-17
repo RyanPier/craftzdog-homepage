3773b6af5becec95d1c08b52e9f02ce77817bff2
RyanPier <136802720+RyanPier@users.noreply.github.com>
2025-09-17 23:32:10 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,98 @@
-<Box display={{ md: 'flex' }}>
-  <Box flexGrow={1}>
-    <Heading as="h2" variant="page-title">
-      Nhat An Vo
-    </Heading>
-    <p>Office Clerk</p>
-  </Box>
-  <Box
-    flexShrink={0}
-    mt={{ base: 4, md: 0 }}
-    ml={{ md: 6 }}
-    textAlign="center"
-  >
-    <Image
-      borderColor="whiteAlpha.800"
-      borderWidth={2}
-      borderStyle="solid"
-      maxWidth="100px"
-      display="inline-block"
-      borderRadius="full"
-      src="/images/nhatanvo.jpg" // Note: You'll need to upload a profile picture named 'nhatanvo.jpg' to the 'public/images/' folder later.
-      alt="Profile image"
-    />
-  </Box>
-</Box>
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  Image,
+  Button,
+  List,
+  ListItem,
+  Icon,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
+import Layout from '../components/layouts/article'
+import Section from '../components/section'
+import { IoLogoGithub } from 'react-icons/io5'
+
+const Home = () => (
+  <Layout>
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      >
+        Hello, I&apos;m a clerk based in Australia!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Nhat An Vo
+          </Heading>
+          <p>Office Clerk</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/nhatanvo.jpg"
+            alt="Profile image"
+          />
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          About Me
+        </Heading>
+        <Paragraph>
+          Midnight enjoyer, negronis.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works" scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/RyanPier" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<Icon as={IoLogoGithub} />}
+              >
+                @RyanPier
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
+    </Container>
+  </Layout>
+)
+
+export default Home
+export { getServerSideProps } from '../components/chakra'
